803ada511ea2bcbb319fdac8a0765a89064b66e6
remicmacs <rembourgeon@gmail.com>
2017-06-08 23:25:22 +0200

    Parsing fini, modèle séisme fini

--- app/src/main/java/us/julesandremi/seismic/MainActivity.java
+++ app/src/main/java/us/julesandremi/seismic/MainActivity.java
@@ -1,8 +1,10 @@
 package us.julesandremi.seismic;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +14,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView tvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +31,13 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        this.tvTest = (TextView) findViewById(R.id.tvTest);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                MainActivity.this.asyncJson();
+                Snackbar.make(view, "Chargement ...", Snackbar.LENGTH_LONG).show();
             }
         });
 
@@ -40,6 +49,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void asyncJson() {
+        new SeismAsyncTask().execute(this.tvTest, this);
+
     }
 
     @Override
