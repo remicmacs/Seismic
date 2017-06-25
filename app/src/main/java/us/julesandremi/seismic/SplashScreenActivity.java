1e7ea7ae8406aff6db3017620054904fd8fec5ea
remicmacs <rembourgeon@gmail.com>
2017-06-25 21:49:39 +0200

    Meilleur Timer vu que ça charge rien

--- app/src/main/java/us/julesandremi/seismic/SplashScreenActivity.java
+++ app/src/main/java/us/julesandremi/seismic/SplashScreenActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
