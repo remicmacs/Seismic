5860ff8555019d09534e6809194244d31e397143
Jules <jules.druguet@etu.univ-lyon1.fr>
2017-06-25 23:55:29 +0200

    GoogleMap

--- app/src/main/java/us/julesandremi/seismic/CompleteMapActivity.java
+++ app/src/main/java/us/julesandremi/seismic/CompleteMapActivity.java
@@ -2,8 +2,11 @@ package us.julesandremi.seismic;
 
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -12,7 +15,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompleteMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private String defaultAddress = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
+    private List listSeism = null;
+    private CustomAdapter seismAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +33,36 @@ public class CompleteMapActivity extends AppCompatActivity implements OnMapReady
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        try{
+            Intent intent =  getIntent();
+            listSeism = (ArrayList<Seism>) intent.getSerializableExtra("list");
+        } catch (Exception err){
+            Log.d("Erreur", "Err");
+        }
+        if (listSeism == null) listSeism = new ArrayList<>();
+        seismAdapter = new CustomAdapter(this, listSeism);
+        this.asyncJson();
+
+    }
+
+    private void asyncJson() {
+        this.asyncJson(this.defaultAddress);
+    }
+
+    private void asyncJson(String url){
+        new SeismAsyncTask().execute(this, this.listSeism, this.seismAdapter, null, url);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+
+        ArrayList <Seism> seismList = (ArrayList) this.seismAdapter.getListSeism();
+        for(Seism seisme : seismList) {
+            googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(seisme.getCoordinates().getLatitude(), seisme.getCoordinates().getLongitude()))
+                    .title(seisme.getTitle()));
+        }
+
     }
 }
